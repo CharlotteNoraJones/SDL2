544aa6e3138aaca25e922b4de72a3b25f4a82036
Charlotte Jones <Charlotte.Jones@armedicalboard.org>
2024-08-01 10:55:21 -0500

    Added sprite to screen.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 use sdl2::event::Event;
+use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
+
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    let (width, height) = canvas.output_size()?;
+
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    canvas.copy(texture, sprite, screen_rect)?;
+
+    canvas.present();
+
+    Ok(())
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
     let window = video_subsystem
         .window("rust-sdl2 demo", 800, 600)
@@ -18,15 +44,15 @@ fn main() -> Result<(), String> {
         .build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let position = Point::new(0, 0);
+    let sprite = Rect::new(0, 0, 26, 36);
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -40,7 +66,16 @@ fn main() -> Result<(), String> {
             }
         }
 
-        canvas.present();
+        i = (i + 1) % 255;
+
+        render(
+            &mut canvas,
+            Color::RGB(i, 64, 255 - i),
+            &texture,
+            position,
+            sprite,
+        )?;
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
